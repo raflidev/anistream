85562c6a0b50fbf0942d1519e4e3db6acbf041a8
Rafli Ramadhan <rafliramdhn@gmail.com>
2020-10-21 17:09:07 +0700

    first commit

---
+++ src/bot.js
@@ -0,0 +1,95 @@
+require("dotenv").config();
+import { getUrl, getListEpisode, getWatch } from "./scrapping";
+
+const { Client } = require("discord.js");
+const client = new Client();
+const PREFIX = "=";
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on("message", async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [cmd, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    var anime = [];
+    if (cmd === "help") {
+      message.channel.send(
+        "```Anistream Help Menu\ngunakan =command\n\ncommands list\nongoing untuk mencari anime musim ini\nnonton```"
+      );
+    } else if (cmd === "ongoing") {
+      message.channel.send("Tunggu sebentar ya..");
+      anime = [];
+      const ongoing = await getUrl();
+      ongoing[0].ongoing.forEach((ep) => {
+        anime.push(`${ep.length}.${ep.judul}`);
+      });
+
+      message.channel.send(anime);
+      message.channel.send(
+        "**Silakan pilih nomor** dengan command **=nonton** nomor, cth: =nonton 7"
+      );
+    } else if (cmd === "nonton") {
+      if (args[0] === undefined) {
+        message.channel.send("Anda belum memasukan ID anime dan ID Episode");
+      } else if (args[1] === undefined) {
+        message.channel.send("Anda belum memasukan ID Episode");
+      } else {
+        message.channel.send("tunggu bentar bang, request time.");
+      }
+
+      const ongoing = await getUrl();
+      ongoing[0].ongoing.forEach((ep) => {
+        anime.push(`${ep.length}.${ep.judul}`);
+      });
+
+      if (args[1] !== undefined) {
+        const episode = await getListEpisode(
+          ongoing[0].ongoing[parseInt(args[0] - 1)].link
+        );
+        const getUrl = await getWatch(episode[parseInt(args[1]) - 1].link);
+        message.channel.send(getUrl);
+        message.channel.send("Selamat menonton, link sudah tersedia!");
+      }
+    } else if (cmd === "episode") {
+      if (args[0] === undefined) {
+        message.channel.send("anda belum memasukan angka");
+      } else {
+        message.channel.send("tunggu bentar bang, request time.");
+      }
+      if (args[0] !== undefined) {
+        const ongoing = await getUrl();
+        ongoing[0].ongoing.forEach((ep) => {
+          anime.push(`${ep.length}.${ep.judul}`);
+        });
+
+        message.channel.send(ongoing[0].ongoing[parseInt(args[0] - 1)].judul);
+        // console.log(args[0]);
+
+        const episode = await getListEpisode(
+          ongoing[0].ongoing[parseInt(args[0] - 1)].link
+        );
+        var arrEpisode = [];
+        arrEpisode = [];
+        episode.forEach((ep) => {
+          arrEpisode.push(`${ep.length}.${ep.judul}`);
+        });
+        try {
+          message.channel.send(arrEpisode);
+        } catch (err) {
+          message.channel.send(err);
+        }
+        message.channel.send(
+          "**Silakan pilih nomor** dengan command **=nonton nomor episode**, cth: =nonton 7 **2**"
+        );
+      }
+    } else if (cmd === "test") {
+      message.channel.send("https://www.youtube.com/embed/hdmUJss3NV0");
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
---
+++ src/scrapping.js
@@ -0,0 +1,48 @@
+import cheerio from "cheerio";
+import axios from "axios";
+
+var anime = [];
+export async function getUrl() {
+  anime = [];
+  const link = "https://otakudesu.tv/ongoing-anime/";
+  const result = await axios.get(link);
+  const $ = cheerio.load(result.data);
+  anime.push({
+    date: Date.now(),
+    ongoing: [],
+  });
+
+  $(".detpost").each(function () {
+    anime[0].ongoing.push({
+      length: anime[0].ongoing.length + 1,
+      judul: $(this).find(".jdlflm").html(),
+      tanggal: $(this).find(".newnime").html(),
+      link: $(this).find("a").attr("href"),
+    });
+  });
+  return anime;
+}
+var episode = [];
+export async function getListEpisode(url) {
+  const result = await axios.get(url);
+  const $ = cheerio.load(result.data);
+  episode = [];
+  $(".episodelist li").each(function () {
+    if ($(this).find("a[data-wpel-link=internal]").attr("href") != undefined) {
+      episode.push({
+        length: episode.length + 1,
+        link: $(this).find("a[data-wpel-link=internal]").attr("href"),
+        judul: $(this).find("a[data-wpel-link=internal]").text(),
+        tanggal: $(this).find(".zeebr").text(),
+      });
+    }
+  });
+  return episode;
+}
+
+export async function getWatch(url) {
+  const result = await axios.get(url);
+  const $ = cheerio.load(result.data);
+  const data = $(".responsive-embed-stream").find("iframe").attr("src");
+  return data;
+}
